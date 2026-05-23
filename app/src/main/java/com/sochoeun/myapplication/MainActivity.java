e1354f142c1cc266f8f544e4e91caef91bdeb074
sochoeun khorn <150789500+sochoeun1202@users.noreply.github.com>
2026-05-23 22:49:50 +0700

    feature - btnCall for open Call Phone App - btnSearch for open browser

--- app/src/main/java/com/sochoeun/myapplication/MainActivity.java
+++ app/src/main/java/com/sochoeun/myapplication/MainActivity.java
@@ -1,9 +1,12 @@
 package com.sochoeun.myapplication;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
     Button btnFruit;
     Button btnVegetable;
     Button btnDrink;
+    ImageButton btnCall;
+    ImageButton btnSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +36,41 @@ public class MainActivity extends AppCompatActivity {
         btnFruit = findViewById(R.id.btnFruit);
         btnVegetable = findViewById(R.id.btnVegetable);
         btnDrink = findViewById(R.id.btnDrink);
+
+        btnCall = findViewById(R.id.imgButtonCall);
+        btnSearch = findViewById(R.id.imgButtonSearch);
     }
 
+    // Handler
+    public void btnClickCall(View view){
+        // Log.d("TestClick", "The ImageButton click works!");
+        // 1. The phone number you want to call
+        String phoneNumber = "123456789";
+
+        // 2. Create the Intent to open the dialer
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+
+        // 3. Give the Intent the phone number (must start with "tel:")
+        dialIntent.setData(Uri.parse("tel:" + phoneNumber));
+
+        // 4. Launch the phone app
+        startActivity(dialIntent);
+    }
+
+    public void btnClickSearch(View view){
+        // 1. The URL you want to open
+        // NOTE: It MUST start with "http://" or "https://"
+        String url = "https://www.google.com";
+
+        // 2. Create the Intent to view the webpage
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW);
+
+        // 3. Give the Intent the URL data
+        browserIntent.setData(Uri.parse(url));
+
+        // 4. Launch the browser app
+        startActivity(browserIntent);
+    }
     // Fruit Handler
     public void btnClickFruit(View view){
          showFoodView(btnFruit.getText().toString(),R.drawable.fruits);
